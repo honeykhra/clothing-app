ee496d848579b758e7d8e3497ec0acc773a3afb0
honey <honeykhra@gmail.com>
2020-03-06 13:15:09 +0530

    added title

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <h1>Title</h1>
+        <h1>{title}</h1>
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
