db6665ff52b258e556e72934d7ab4bc36f156ed8
honey <honeykhra@gmail.com>
2020-03-07 22:00:44 +0530

    Added signup page and google auth functionality

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyD0NV_GsVKVnKEpByUyG67cOaK11RbtJYI",
+    authDomain: "clothing-db-db5a3.firebaseapp.com",
+    databaseURL: "https://clothing-db-db5a3.firebaseio.com",
+    projectId: "clothing-db-db5a3",
+    storageBucket: "clothing-db-db5a3.appspot.com",
+    messagingSenderId: "152879594956",
+    appId: "1:152879594956:web:ffda91627dd5892407b169",
+    measurementId: "G-NKQFFMYLC5"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
