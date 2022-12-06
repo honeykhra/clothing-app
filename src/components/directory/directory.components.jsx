fb857886ecde2ed3fe6c10d2ff1737fb911fa43c
honeykhra <honeykhra@gmail.com>
2022-12-06 13:34:06 +0530

    commit message

---
+++ src/components/directory/directory.components.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import MenuItem from '../menu-items/menu-item.components';
+import './directory.styles.scss';
+import SECTIONS_DATA from './sections.data';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: SECTIONS_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({ id, ...otherSectionProps }) =>
+                        (
+                            <MenuItem key={id} {...otherSectionProps} />
+                        ))
+                }
+            </div>
+        )
+    }
+
+}
+
+export default Directory;
