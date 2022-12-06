63c526cac7e63c2d88c70896284a8acc97dd4915
honeykhra <honeykhra@gmail.com>
2022-12-06 13:52:08 +0530

    errors fixed

--- src/components/menu-items/menu-item.components.jsx
+++ src/components/menu-items/menu-item.components.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import './menu-item.styles.scss'
-
+import React from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
-    </div >
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
 );
 
-export default withRouter(MenuItem);
+export default function withRouter(MenuItem) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <MenuItem {...props} router={{ location, navigate, params }} />;
+  }
+
+  return ComponentWithRouterProp;
+}
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-
+// v9 compat packages are API compatible with v8 code
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const config = {
-    apiKey: "AIzaSyD0NV_GsVKVnKEpByUyG67cOaK11RbtJYI",
-    authDomain: "clothing-db-db5a3.firebaseapp.com",
-    databaseURL: "https://clothing-db-db5a3.firebaseio.com",
-    projectId: "clothing-db-db5a3",
-    storageBucket: "clothing-db-db5a3.appspot.com",
-    messagingSenderId: "152879594956",
-    appId: "1:152879594956:web:ffda91627dd5892407b169",
-    measurementId: "G-NKQFFMYLC5"
+  apiKey: "AIzaSyD0NV_GsVKVnKEpByUyG67cOaK11RbtJYI",
+  authDomain: "clothing-db-db5a3.firebaseapp.com",
+  databaseURL: "https://clothing-db-db5a3.firebaseio.com",
+  projectId: "clothing-db-db5a3",
+  storageBucket: "clothing-db-db5a3.appspot.com",
+  messagingSenderId: "152879594956",
+  appId: "1:152879594956:web:ffda91627dd5892407b169",
+  measurementId: "G-NKQFFMYLC5",
 };
 
 firebase.initializeApp(config);
@@ -20,7 +20,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
